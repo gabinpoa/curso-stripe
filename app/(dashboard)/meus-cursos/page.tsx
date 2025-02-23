3f739a36d66dbf264faca41cea5e869cc01e1070
Gabriel Nascimento <99542800+gabinpoa@users.noreply.github.com>
2025-02-23 20:24:03 -0300

    fix center course card img

--- app/(dashboard)/meus-cursos/page.tsx
+++ app/(dashboard)/meus-cursos/page.tsx
@@ -6,18 +6,10 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
-import Image from 'next/image';
 import Link from 'next/link';
 import { Badge } from '@/components/ui/badge';
-import {
-  getCustomerValidSubscriptions,
-  getExpandedCustomerValidSubscriptions,
-} from '@/lib/payments/stripe';
-import {
-  getCustomerId,
-  getProductsModulesPreview,
-  getUserWithTeam,
-} from '@/lib/db/queries';
+import { getExpandedCustomerValidSubscriptions } from '@/lib/payments/stripe';
+import { getCustomerId, getProductsModulesPreview } from '@/lib/db/queries';
 
 export default async function PaginaMeusCursos() {
   const customerId = await getCustomerId();
@@ -49,41 +41,59 @@ export default async function PaginaMeusCursos() {
       <h1 className="text-3xl font-bold mb-6">Meus Cursos</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {subscribedCourses.map((course) => (
-          <Card key={course.id} className="flex flex-col">
-            <CardHeader>
-              <img
-                src={course.images[0] || '/static/placeholder.png'}
-                alt={course.name}
-                width={300}
-                height={150}
-                className="rounded-md object-cover"
-              />
-            </CardHeader>
-            <CardContent className="flex-grow">
-              <CardTitle className="mb-2">{course.name}</CardTitle>
-              <p className="text-sm text-gray-600 mb-4">{course.description}</p>
-              <div className="space-y-2">
-                {course.modules.map((module, index) => (
-                  <div
-                    key={index}
-                    className="flex items-center justify-between"
-                  >
-                    <span className="text-sm">{module.name}</span>
-                    {module.isExtraContent && (
-                      <Badge variant="secondary">Extra</Badge>
-                    )}
-                  </div>
-                ))}
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Link href={`/cursos/${course.id}`} passHref className="w-full">
-                <Button className="w-full">Acessar Curso</Button>
-              </Link>
-            </CardFooter>
-          </Card>
+          <CourseCard key={course.id} {...course} />
         ))}
       </div>
     </div>
+  );
+}
+
+interface CourseCardProps {
+  id: string;
+  name: string;
+  description: string | null;
+  images: string[];
+  modules: {
+    id: number;
+    productId: string;
+    name: string;
+    description: string | null;
+    order: number;
+    isExtraContent: boolean;
+  }[];
+}
+
+function CourseCard(course: CourseCardProps) {
+  return (
+    <Card key={course.id} className="flex flex-col">
+      <CardHeader className="items-center">
+        <img
+          src={course.images[0] || '/static/placeholder.png'}
+          alt={course.name}
+          width={300}
+          height={150}
+          className="rounded-md object-cover "
+        />
+      </CardHeader>
+      <CardContent className="flex-grow">
+        <CardTitle className="mb-2">{course.name}</CardTitle>
+        <p className="text-sm text-gray-600 mb-4">{course.description}</p>
+        <div className="space-y-2">
+          {course.modules.map((module, index) => (
+            <div key={index} className="flex items-center justify-between">
+              <span className="text-sm">{module.name}</span>
+              {module.isExtraContent && (
+                <Badge variant="secondary">Extra</Badge>
+              )}
+            </div>
+          ))}
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Link href={`/cursos/${course.id}`} passHref className="w-full">
+          <Button className="w-full">Acessar Curso</Button>
+        </Link>
+      </CardFooter>
+    </Card>
   );
 }
