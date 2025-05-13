ea3280a6d5e77ab7dfb3b3df6813f52fa5098221
Gabriel Nascimento <99542800+gabinpoa@users.noreply.github.com>
2025-05-13 20:01:24 -0300

    send email with https api instead of direct smtp

--- lib/email/send.ts
+++ lib/email/send.ts
@@ -1,20 +1,16 @@
-import nodemailer from 'nodemailer';
-
 export async function sendEmail(to: string, subject: string, html: string) {
     if (!process.env.SMTP_HOST || !process.env.SMTP_PORT || !process.env.SMTP_USER || !process.env.SMTP_PASS || !process.env.SMTP_FROM) {
         throw new Error('SMTP configuration is missing in environment variables.');
     }
 
     const smtpPort = parseInt(process.env.SMTP_PORT, 10);
-    const transporter = nodemailer.createTransport({
+    const smtpTransportOptions = {
         host: process.env.SMTP_HOST,
         port: smtpPort,
         secure: smtpPort === 465, // true for 465, false for other ports
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS,
-        }
-    });
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+    };
 
     const mailOptions = {
         from: process.env.SMTP_FROM,
@@ -24,8 +20,20 @@ export async function sendEmail(to: string, subject: string, html: string) {
     };
 
     try {
-        const info = await transporter.sendMail(mailOptions);
-        console.log('Email sent:', info.response);
+        const response = await fetch('https://smtp-api-layer.vercel.app/api/smtp-send', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                smtpTransportOptions,
+                mailOptions,
+            })
+        });
+        if (!response.ok || response.status !== 200) {
+            throw new Error(`Failed to send email: ${response.statusText}`);
+        }
+        console.log('Email sent:', await response.json());
     } catch (error) {
         console.error('Error sending email:', error);
         throw error;
