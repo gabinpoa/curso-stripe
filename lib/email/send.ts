5133e77c923fe8479632ed3e5acef30b9a0d57e4
Gabriel Nascimento <99542800+gabinpoa@users.noreply.github.com>
2025-05-14 04:05:04 -0300

    adapt api to kinghost smtp transacional

--- lib/email/send.ts
+++ lib/email/send.ts
@@ -1,38 +1,42 @@
 export async function sendEmail(to: string, subject: string, html: string) {
-    if (!process.env.SMTP_HOST || !process.env.SMTP_PORT || !process.env.SMTP_USER || !process.env.SMTP_PASS || !process.env.SMTP_FROM) {
-        throw new Error('SMTP configuration is missing in environment variables.');
+    if (!process.env.SMTP_FROM) {
+        throw new Error('SMTP_FROM is missing in environment variables.');
+    }
+    if (!process.env.SMTP_API_URL) {
+        throw new Error('SMTP_API_URL is missing in environment variables.');
+    }
+    if (!process.env.SMTP_AUTH_TOKEN) {
+        throw new Error('SMTP_AUTH_TOKEN is missing in environment variables.');
     }
 
-    const smtpPort = parseInt(process.env.SMTP_PORT, 10);
-    const smtpTransportOptions = {
-        host: process.env.SMTP_HOST,
-        port: smtpPort,
-        secure: smtpPort === 465, // true for 465, false for other ports
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-    };
+    const apiUrl = process.env.SMTP_API_URL + "/v1/messages";
+    const authToken = process.env.SMTP_AUTH_TOKEN;
 
-    const mailOptions = {
-        from: process.env.SMTP_FROM,
-        to,
+    const emailPayload = {
         subject,
-        html
+        body: html,
+        to,
+        from: process.env.SMTP_FROM,
+        headers: {
+            "Content-Type": "text/plain; charset=us-ascii",
+        }
     };
 
     try {
-        const response = await fetch('https://smtp-api-layer.vercel.app/api/smtp-send', {
+        const response = await fetch(apiUrl, {
             method: 'POST',
             headers: {
+                'accept': 'application/json',
                 'Content-Type': 'application/json',
+                'x-auth-token': authToken
             },
-            body: JSON.stringify({
-                smtpTransportOptions,
-                mailOptions,
-            })
+            body: JSON.stringify(emailPayload)
         });
+
         if (!response.ok || response.status !== 200) {
             throw new Error(`Failed to send email: ${response.statusText}`);
         }
+
         console.log('Email sent:', await response.json());
     } catch (error) {
         console.error('Error sending email:', error);
