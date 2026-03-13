431c4ecc052d763c3a86b63f2c6d4260e41ee40b
Gabriel Nascimento <gabrielnpoa@gmail.com>
2026-03-12 21:16:09 -0300

    Remove uneeded remote pattern for next/image

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'files.stripe.com',
-        port: '',
-        pathname: '/links/**',
-        search: '',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
